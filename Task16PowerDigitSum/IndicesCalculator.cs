65a624a37c292f401da270d89d50f81a2b8dd4bf
Jamie Acharya <Jamieacharya@jamieacharya.com>
2023-04-18 12:53:12 +0100

    Task 16 - Power Digit Sum

---
+++ Task16PowerDigitSum/IndicesCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace Task16PowerDigitSum
+{
+    /// <summary>
+    /// Calculates indicies
+    /// </summary>
+    internal class IndicesCalculator
+    {
+        /// <summary>
+        /// Calculates an indicies when given a base number and an index.
+        /// </summary>
+        /// <param name="baseNumber">The base number.</param>
+        /// <param name="index">The index.</param>
+        /// <returns>string</returns>
+        public static string CalculateIndex(int baseNumber, int index)
+        {
+            BigInteger result = BigInteger.Pow(baseNumber, index);
+            
+            return result.ToString();
+        }
+    }
+}
---
+++ Task16PowerDigitSum/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task16PowerDigitSum
+{
+    /// <summary>
+    /// Main Entry Point
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project Euler TASK 16 - Power Digit Sum\n");
+
+            Console.WriteLine("Please enter a base number: ");
+
+            int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the index/power: ");
+
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(IndicesCalculator.CalculateIndex(baseNumber, index));
+        }
+    }
+}
