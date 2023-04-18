613b1264b9c0ec0e08f8916de76873bb4ad411ed
Jamie Acharya <Jamieacharya@jamieacharya.com>
2023-04-18 11:06:14 +0100

    Task 20 - Code Refactor

--- Task20FactorialDigitSum/SumOfDigits.cs
+++ Task20FactorialDigitSum/SumOfDigits.cs
@@ -1,4 +1,6 @@
-﻿namespace Task20FactorialDigitSum
+﻿using System.Diagnostics.Metrics;
+
+namespace Task20FactorialDigitSum
 {
     /// <summary>
     /// Calculates the summanation for the digits of a given positive natural number
@@ -13,8 +15,18 @@
         public static string CalculateSumOfDigits(string value)
         {
             //Original - Converted Big Integer to string then iterated over string characters
+            int count, result = 0;
+            int numbers = value.Length;
 
-            int result = value.Sum(x => x - '0'); //'0' ASCII = 48, Sums the digits minus the original length of the string, non-original but interesting implementiation
+            for (count = 0; count < numbers; count++)
+            {
+                if ((value[count] >= '0') && (value[count] <= '9'))
+                {
+                    result += (value[count] - '0');
+                }
+            }
+
+            //int result = value.Sum(x => x - '0'); //'0' ASCII = 48, Sums the digits minus the original length of the string, non-original but interesting implementiation
             //Non-Linq
             //foreach (Char c in BigNumberStr)
             //    Sum += (byte)c;
