0ba8beeb4812b5ec78955374b9922344eccb0bec
Jamie Acharya <Jamieacharya@jamieacharya.com>
2023-04-18 12:57:29 +0100

    Task 09 - Special Pythagorean Triplet

---
+++ Task09SpecialPythagoreanTriplet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task09SpecialPythagoreanTriplet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project Euler TASK 09 - Special Pythagorean Triplet \n");
+        }
+    }
+}
