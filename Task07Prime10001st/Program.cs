7e85ffcd2e64ab1999f7c3a47a32f7038b265184
Jamie Acharya <Jamieacharya@jamieacharya.com>
2023-04-18 03:41:34 +0100

    Code refactor of Task 07

--- Task07Prime10001st/Program.cs
+++ Task07Prime10001st/Program.cs
@@ -12,13 +12,15 @@
             Console.WriteLine("Enter '1' to run Prime Position Finder\nEnter '2' to calculate if a number is prime and its position");
 
             int choice = Convert.ToInt32(Console.ReadLine());
+
             if (choice == 1)
             {
                 Console.WriteLine("Please enter a number to find that positions prime: ");
 
                 int position = Convert.ToInt32(Console.ReadLine());
 
-                PrimePositionCalculator primePositionCalculator = new();
+                _ = new PrimePositionCalculator(); //Won't use the assigned variable so can implement discard
+
                 var calculatedPrime = PrimePositionCalculator.CalculatePrimePosition(position);
                 Console.WriteLine("The {0} prime is: {1}", position, calculatedPrime);
             }
@@ -26,7 +28,9 @@
             {
                 Console.WriteLine("Please enter a number to find if it is a prime and its position: ");
                 int position = Convert.ToInt32(Console.ReadLine());
-                PrimeCalculator primeCalculator = new();
+                
+                _ = new PrimeCalculator();
+                
                 var isPrimeandPosition = PrimeCalculator.CalculatePrime(position);
                 Console.WriteLine(isPrimeandPosition);            
             }
