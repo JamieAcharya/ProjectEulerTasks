72946cbfe49c8ef4ed69adef73bcb5ca039efd3b
Jamie Acharya <Jamieacharya@jamieacharya.com>
2023-04-18 04:51:30 +0100

    Task 20 - Refactor

--- Task20FactorialDigitSum/SumOfDigits.cs
+++ Task20FactorialDigitSum/SumOfDigits.cs
@@ -12,8 +12,16 @@
         /// <returns>string</returns>
         public static string CalculateSumOfDigits(string value)
         {
-            //orignial was mod sum method
+            //Original - Converted Big Integer to string then iterated over string characters
+
             int result = value.Sum(x => x - '0'); //'0' ASCII = 48, Sums the digits minus the original length of the string, non-original but interesting implementiation
+            //Non-Linq
+            //foreach (Char c in BigNumberStr)
+            //    Sum += (byte)c;
+
+            // 48 is ascii value of zero
+            // remove in one step rather than in the loop
+            //Sum -= 48 * BigNumberStr.Length;
 
             return result.ToString();
         }
